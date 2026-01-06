71d743c4902262b270108e9f02bedafe592d71bf
VitorBaldasso <vitor.baldasso05@gmail.com>
2026-01-06 14:23:56 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Banner } from './Banner'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <header>
+        <Banner/>
+      </header>
+      
+
+    </div>
+    
+  )
+}
+
+export default App
---
+++ src/Banner.jsx
@@ -0,0 +1,12 @@
+import styles from './Banner.module.css'
+import bannerzin from './assets/banner.jpeg'
+
+export function Banner(){
+    return(
+        <header>
+            <header className={styles.header}>
+                <img src={bannerzin} alt="Logotipo-Banner" />
+            </header>
+        </header>
+    )
+}
